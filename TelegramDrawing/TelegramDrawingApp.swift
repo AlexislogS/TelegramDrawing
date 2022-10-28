230178073be03cf5b90d380b70d0327d239b6bd2
Alex Yatsenko <alexislog2@gmail.com>
2022-10-29 00:43:04 +0300

    Initial Commit

---
+++ TelegramDrawing/TelegramDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramDrawingApp.swift
+//  TelegramDrawing
+//
+//  Created by Alex Yatsenko on 29.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TelegramDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
